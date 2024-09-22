8eba77a1a1ea86cc04092d38ae35411e5133d9f0
Rashmi <rashmantri2003@gmail.com>
2024-09-22 18:25:11 +0530

    Updated RTE file

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,54 +1,78 @@
-import React from 'react'
-import {Editor } from '@tinymce/tinymce-react';
-import {Controller } from 'react-hook-form';
+import React from "react"
+import { Editor } from "@tinymce/tinymce-react"
+import { Controller } from "react-hook-form"
 
+export default function RTE({ name, control, label, defaultValue = "" }) {
+	return (
+		<div className="w-full">
+			{label && <label className="inline-block mb-1 pl-1">{label}</label>}
 
-export default function RTE({name, control, label, defaultValue =""}) {
-  return (
-    <div className='w-full'> 
-    {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
-
-    <Controller
-    name={name || "content"}
-    control={control}
-    render={({field: {onChange}}) => (
-        <Editor
-        initialValue={defaultValue}
-        init={{
-            initialValue: defaultValue,
-            height: 500,
-            menubar: true,
-            plugins: [
-                "image",
-                "advlist",
-                "autolink",
-                "lists",
-                "link",
-                "image",
-                "charmap",
-                "preview",
-                "anchor",
-                "searchreplace",
-                "visualblocks",
-                "code",
-                "fullscreen",
-                "insertdatetime",
-                "media",
-                "table",
-                "code",
-                "help",
-                "wordcount",
-                "anchor",
-            ],
-            toolbar:
-            "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
-            content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
-        }}
-        onEditorChange={onChange}
-        />
-    )}
-    />
-
-     </div>
-  )
+			<Controller
+				name={name || "content"}
+				control={control}
+				render={({ field: { onChange } }) => (
+					<Editor
+						apiKey="shjrr4vrizauoup1c6zqnlc7hq1ugccpc83bm4sh4wjx78bi"
+						init={{
+							plugins: [
+								// Core editing features
+								"anchor",
+								"autolink",
+								"charmap",
+								"codesample",
+								"emoticons",
+								"image",
+								"link",
+								"lists",
+								"media",
+								"searchreplace",
+								"table",
+								"visualblocks",
+								"wordcount",
+								// Your account includes a free trial of TinyMCE premium features
+								// Try the most popular premium features until Oct 6, 2024:
+								"checklist",
+								"mediaembed",
+								"casechange",
+								"export",
+								"formatpainter",
+								"pageembed",
+								"a11ychecker",
+								"tinymcespellchecker",
+								"permanentpen",
+								"powerpaste",
+								"advtable",
+								"advcode",
+								"editimage",
+								"advtemplate",
+								"ai",
+								"mentions",
+								"tinycomments",
+								"tableofcontents",
+								"footnotes",
+								"mergetags",
+								"autocorrect",
+								"typography",
+								"inlinecss",
+								"markdown",
+							],
+							toolbar:
+								"undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
+							tinycomments_mode: "embedded",
+							tinycomments_author: "Author name",
+							mergetags_list: [
+								{ value: "First.Name", title: "First Name" },
+								{ value: "Email", title: "Email" },
+							],
+							ai_request: (request, respondWith) =>
+								respondWith.string(() =>
+									Promise.reject("See docs to implement AI Assistant")
+								),
+						}}
+						initialValue=""
+					/>
+				)}
+			/>
+		</div>
+	)
 }
